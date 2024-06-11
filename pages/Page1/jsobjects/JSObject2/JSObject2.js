fa2cca47eee68e7568c5f481cf5363e6b5988da2
Aparna Ramachandran <aparna@appsmith.com>
2024-06-11 07:25:45 +0000

    new js object

---
+++ pages/Page1/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,14 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		
+		storeValue( )
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
